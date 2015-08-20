ce41d9595a71fe1b63c00a1421d9c0c59cb6fe55
Denis Stebunov <denis.stebunov@ivelum.com>
2015-08-20 15:30:34 +0300

    upload to PyPI

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+import graphql
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
+
+packages = ['graphql']
+requires = ['ply>=3.6']
+
+setup(
+    name='qraphql-py',
+    version=graphql.__version__,
+    description='graphql-py: Parser for latest GraphQL specification',
+    long_description=open('readme.md').read(),
+    author='Denis Stebunov',
+    author_email='support@ivelum.com',
+    url='https://github.com/ivelum/graphql-py/',
+    packages=packages,
+    install_requires=requires,
+    license=open('LICENSE').read(),
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ),
+)
+
+del os.environ['PYTHONDONTWRITEBYTECODE']
